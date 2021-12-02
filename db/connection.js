5aa5a8303362b21193b763eea4abd477a1867d51
HeatherEver <heather.eva93@gmail.com>
2021-12-02 13:04:23 +0000

    starting again

--- db/connection.js
+++ db/connection.js
@@ -5,17 +5,17 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set or DATABASE_URL is not set');
 }
-const config =
-  ENV === 'production'
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
+// const config =
+//   ENV === 'production'
+//     ? {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       }
+//     : {};
 
-module.exports = new Pool(config);
+module.exports = new Pool();
